5b3471d5429637ea83ccf9da6b55117937bf16d9
Nidhi Agrawal <agrawal.nidhi6@gmail.com>
2020-11-26 14:48:29 -0800

    WI3:Singleton pattern of spring container using XML configrution Application Context

--- SpringDemo1/src/main/java/com/learning/springdemo1/App.java
+++ SpringDemo1/src/main/java/com/learning/springdemo1/App.java
@@ -18,9 +18,17 @@ public class App {
 		ApplicationContext factory = new ClassPathXmlApplicationContext("springconfig.xml");
 		
 		//bean created in springconfig.xml file bean id is stu1
-		Student obj =(Student)factory.getBean("stu1");
-		 
-		obj.study();
+		Student obj1 =(Student)factory.getBean("stu1");
+		obj1.study();
+		obj1.age=20;
+		System.out.println("Age of Obj1 is :"+obj1.age);
+		
+		Student obj2 =(Student)factory.getBean("stu1");
+		obj2.study();
+		System.out.println("Age of Obj2 is :"+obj2.age);
+		
+		//obj2 will also pring age as 20 as Object is created only 1 i.e stu1 referneces are two obj1 & obj 2
+		//Spring Container is creating only one object (stu1) --This is known as Singleton pattern 
 	}
 
 }
--- SpringDemo1/src/main/java/com/learning/springdemo1/Student.java
+++ SpringDemo1/src/main/java/com/learning/springdemo1/Student.java
@@ -1,6 +1,11 @@
 package com.learning.springdemo1;
 
 public class Student {
+	int age;
+	public Student()
+	{
+		System.out.println("Student Object is created");
+	}
 	
 	public void study()
 	{
