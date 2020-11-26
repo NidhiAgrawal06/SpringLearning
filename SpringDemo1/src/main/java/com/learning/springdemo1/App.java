cf5980d192e577f1366975ede966aa5563e77600
Nidhi Agrawal <agrawal.nidhi6@gmail.com>
2020-11-26 14:56:27 -0800

    WI4:Prototype pattern of spring container using XML configrution

--- SpringDemo1/src/main/java/com/learning/springdemo1/App.java
+++ SpringDemo1/src/main/java/com/learning/springdemo1/App.java
@@ -26,9 +26,11 @@ public class App {
 		Student obj2 =(Student)factory.getBean("stu1");
 		obj2.study();
 		System.out.println("Age of Obj2 is :"+obj2.age);
-		
-		//obj2 will also pring age as 20 as Object is created only 1 i.e stu1 referneces are two obj1 & obj 2
+		//If Scope is not specified
+		//obj2 will also print age as 20 as Object is created only 1 i.e stu1 referneces are two obj1 & obj 2
 		//Spring Container is creating only one object (stu1) --This is known as Singleton pattern 
+	
+		//If scope is specified as prototype is will different age for obj1 and obj 2
 	}
 
 }
