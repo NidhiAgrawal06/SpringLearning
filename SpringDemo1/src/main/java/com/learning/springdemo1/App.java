c00788a372a8816bdc84f038d4ffbc24594d6e9b
Nidhi Agrawal <agrawal.nidhi6@gmail.com>
2020-11-26 14:30:40 -0800

    WI2:Spring Configrution through XML file using Application Context

--- SpringDemo1/src/main/java/com/learning/springdemo1/App.java
+++ SpringDemo1/src/main/java/com/learning/springdemo1/App.java
@@ -2,13 +2,20 @@ package com.learning.springdemo1;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 public class App {
 
 	public static void main(String[] args) {
 		//XML Based Configuration file 
-		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("springconfig.xml"));
+		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("springconfig.xml"));
+		
+		
+		//using application context for XML based Configuration File 
+		//Note: XML File should be in class path 
+		ApplicationContext factory = new ClassPathXmlApplicationContext("springconfig.xml");
 		
 		//bean created in springconfig.xml file bean id is stu1
 		Student obj =(Student)factory.getBean("stu1");
