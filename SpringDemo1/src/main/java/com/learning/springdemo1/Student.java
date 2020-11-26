239488c86a271d3cf76baee78dcb9280ef49bbc4
Nidhi Agrawal <agrawal.nidhi6@gmail.com>
2020-11-26 15:19:41 -0800

    WI5:Setter Injection for primitive datatype using XML configrution

--- SpringDemo1/src/main/java/com/learning/springdemo1/Student.java
+++ SpringDemo1/src/main/java/com/learning/springdemo1/Student.java
@@ -1,11 +1,22 @@
 package com.learning.springdemo1;
 
 public class Student {
-	int age;
+	private int age;
+	
 	public Student()
 	{
 		System.out.println("Student Object is created");
 	}
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+		System.out.println("Age assigned");
+	}
+
+	
 	
 	public void study()
 	{
